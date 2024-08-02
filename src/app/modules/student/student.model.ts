4d7cb8a328df583cd7ab21e20fe244a3a521e0e2
Md Rabby <ablazerabbi159@gmail.com>
2024-08-03 02:45:41 +0600

    creating student interface and model

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose'
+import { Guardians, Student, UserName } from './student.interface'
+
+const guardiansSchema = new Schema<Guardians>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+})
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+})
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String },
+  permanentAddress: { type: String },
+  guardians: guardiansSchema,
+  profileImg: { type: String, required: true },
+  isActive: ['active', 'irregular'],
+});
+
+const Student = model<Student>('Student', studentSchema);
