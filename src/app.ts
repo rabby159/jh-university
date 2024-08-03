c7ab9d95a727d15027d4e2b7c8ca7f7cda02b8dd
Md Rabby <ablazerabbi159@gmail.com>
2024-08-03 21:45:45 +0600

    done crud operation

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express, { Application, Request, Response } from 'express'
-import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-const app : Application = express();
+import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
+const app: Application = express()
 
 //parser
-app.use(express.json());
+app.use(express.json())
 app.use(cors())
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
+app.use('/api/v1/students', StudentRoutes)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Rabby Jr!')
+  res.send('Md Rabby')
 })
 
 export default app
--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,30 +1,29 @@
-import { } from 'mongoose';
+import {} from 'mongoose'
 
 export type Guardians = {
-    fatherName : string;
-    fatherOccupation : string;
-    motherName : string;
-    motherOccupation : string;
+  fatherName: string
+  fatherOccupation: string
+  motherName: string
+  motherOccupation: string
 }
 
 export type UserName = {
-    firstName : string;
-    middleName : string;
-    lastName : string;
+  firstName: string
+  middleName: string
+  lastName: string
 }
 
-
 export type Student = {
-    id : string;
-    name: UserName;
-    gender : 'male' | 'female';
-    dateOfBirth? : string;
-    email: string;
-    contactNo : string;
-    bloodGroup? : "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
-    presentAddress : string;
-    permanentAddress : string;
-    guardians : Guardians;
-    profileImg? : string;
-    isActive : 'active' | 'irregular';
-  }
+  id: string
+  name: UserName
+  gender: 'male' | 'female'
+  dateOfBirth?: string
+  email: string
+  contactNo: string
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  presentAddress: string
+  permanentAddress: string
+  guardians: Guardians
+  profileImg?: string
+  isActive: 'active' | 'irregular'
+}
