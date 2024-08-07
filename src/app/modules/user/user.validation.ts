c3018a643ac75ae2f42249381d3402fb3a3527fd
Md Rabby <ablazerabbi159@gmail.com>
2024-08-07 11:42:07 +0600

    refector model and user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
 const userValidationSchema = z.object({
-  id: z.string(),
-  password: z.string().max(15, { message: 'Not more then 15 character' }),
-  needPasswordChange: z.boolean().optional().default(true),
-  role: z.enum(['student', 'faculty', 'admin']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().optional().default(false),
+  password: z
+    .string({
+      invalid_type_error: 'Name must be a string',
+    })
+    .max(15, { message: 'Not more then 15 character' })
+    .optional(),
 })
 
 export const UserValidation = {
