482cab1fd9af58c14a09e50a3337d00018755107
Md Rabby <ablazerabbi159@gmail.com>
2024-08-15 02:45:56 +0600

    academicFaculty validation complete

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -1,13 +1,18 @@
 import { TAcademicFaculty } from './academicFaculty.interface'
 import { model, Schema } from 'mongoose'
 
-const academicFacultySchema = new Schema<TAcademicFaculty>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-})
+  {
+    timestamps: true,
+  },
+)
 
 export const AcademicFaculty = model<TAcademicFaculty>(
   'AcademicFaculty',
