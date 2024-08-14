8705d5f6ebd364c2abb478d728e68a172bd8d94f
Md Rabby <ablazerabbi159@gmail.com>
2024-08-15 02:39:37 +0600

    academicFaculty model create

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,15 @@
+import { TAcademicFaculty } from './academicFaculty.interface'
+import { model, Schema } from 'mongoose'
+
+const academicFacultySchema = new Schema<TAcademicFaculty>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+})
+
+export const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+)
