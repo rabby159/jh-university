3bafb01fd35f403e154d722cde71e2145a351eb1
Md Rabby <ablazerabbi159@gmail.com>
2024-08-15 03:12:59 +0600

    academic faculty controller CRUD operation

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,64 @@
+import httpStatus from 'http-status'
+import catchAsync from '../../utils/catchAsync'
+import sendResponse from '../../utils/sendResponse'
+import { AcademicFacultyServices } from './academicFaculty.service'
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  //will we call services function to send this data
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
+    req.body,
+  )
+
+  //send response
+  res.status(200).json({
+    success: true,
+    message: 'Academic faculty is created successfully',
+    data: result,
+  })
+})
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB()
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty are retrieved successfully',
+    data: result,
+  })
+})
+
+const getSingleAcademicSemester = catchAsync(async (req, res) => {
+  const { facultyId } = req.params
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved succesfully',
+    data: result,
+  })
+})
+
+const updateAcademicSemester = catchAsync(async (req, res) => {
+  const { semesterId } = req.params
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    semesterId,
+    req.body,
+  )
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved succesfully',
+    data: result,
+  })
+})
+
+export const AcademicFacultyControllers = {
+  createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicSemester,
+  updateAcademicSemester,
+}
