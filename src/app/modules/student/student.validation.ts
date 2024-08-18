3da058cdd5c75f19a3bbbf90d34ca5459504926c
Md Rabby <ablazerabbi159@gmail.com>
2024-08-19 03:30:18 +0600

    update validation create for student

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -50,6 +50,73 @@ const createStudentValidationSchema = z.object({
   }),
 })
 
+// Define Zod schema for Guardians
+const updateGuardiansValidationSchema = z.object({
+  fatherName: z.string().min(1, "Father's name is required").optional(),
+  fatherOccupation: z
+    .string()
+    .min(1, "Father's occupation is required")
+    .optional(),
+  motherName: z.string().min(1, "Mother's name is required").optional(),
+  motherOccupation: z
+    .string()
+    .min(1, "Mother's occupation is required")
+    .optional(),
+})
+
+// Define Zod schema for UserName
+const updateUserNameValidationSchema = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(1, 'First name is must required')
+    .max(15, 'First name maximum length is 15')
+    .optional(),
+  middleName: z.string().trim().optional(),
+  lastName: z
+    .string()
+    .trim()
+    .min(1, 'Last name is required')
+    .max(15, 'Last name maximum length is 15')
+    .optional(),
+})
+
+const updateStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(15).optional(),
+    student: z.object({
+      name: updateUserNameValidationSchema,
+      gender: z
+        .enum(['male', 'female'], {
+          errorMap: () => ({ message: 'Invalid gender value' }),
+        })
+        .optional(),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email('Invalid email address').optional(),
+      contactNo: z.string().min(1, 'Contact number is required').optional(),
+      bloodGroup: z
+        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+        .optional(),
+      presentAddress: z
+        .string()
+        .min(1, 'Present Address is required')
+        .optional(),
+      permanentAddress: z
+        .string()
+        .min(1, 'Permanent Address is required')
+        .optional(),
+      guardians: updateGuardiansValidationSchema,
+      profileImg: z
+        .string()
+        .url('Invalid URL for profile image')
+        .min(1, 'Profile image URL is required')
+        .optional(),
+      admissionSemester: z.string().optional(),
+    }),
+  }),
+})
+
 export const studentValidations = {
   createStudentValidationSchema,
+  updateStudentValidationSchema,
 }
