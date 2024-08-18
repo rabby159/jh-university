5d6405153f23be64783614a33766bd9908096ddc
Md Rabby <ablazerabbi159@gmail.com>
2024-08-19 02:25:18 +0600

    trasaction rollback in user service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import config from '../../config'
 import { TStudent } from '../student/student.interface'
 import { Student } from '../student/student.model'
@@ -5,6 +6,8 @@ import { AcademicSemester } from './../academicSemester/academicSemester.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 import { generateStudentId } from './user.utils'
+import AppError from '../../errors/appError'
+import httpStatus from 'http-status'
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user object
@@ -26,20 +29,42 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     throw new Error('Admission semester not found')
   }
 
-  //set  generated id
-  userData.id = await generateStudentId(admissionSemester)
+  //start session
+  const session = await mongoose.startSession()
 
-  // create a user
-  const newUser = await User.create(userData)
+  try {
+    //start transaction
+    session.startTransaction()
 
-  //create a student
-  if (Object.keys(newUser).length) {
+    //set  generated id
+    userData.id = await generateStudentId(admissionSemester)
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session })
+
+    //create a student
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user')
+    }
     // set id , _id as user
-    payload.id = newUser.id
-    payload.user = newUser._id //reference _id
+    payload.id = newUser[0].id
+    payload.user = newUser[0]._id //reference _id
 
-    const newStudent = await Student.create(payload)
+    // create a student (transaction-2)
+    const newStudent = await Student.create([payload, { session }])
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+
     return newStudent
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
   }
 }
 
