75f9fad11f3f7b658b4fd3b25c7b5109974eb5d7
Md Rabby <ablazerabbi159@gmail.com>
2024-08-21 21:33:35 +0600

    implement filter and sort

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -21,7 +21,7 @@ class QueryBuilder<T> {
       });
     };
 
-    filter() {
+    filter(){
         const queryObj = { ...this.query }; // copy
     
         // Filtering
@@ -39,6 +39,24 @@ class QueryBuilder<T> {
           (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
         this.modelQuery = this.modelQuery.sort(sort as string);
     
+        return this;
+      }
+
+      paginate() {
+        const page = Number(this?.query?.page) || 1;
+        const limit = Number(this?.query?.limit) || 10;
+        const skip = (page - 1) * limit;
+    
+        this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+    
+        return this;
+      }
+    
+      fields() {
+        const fields =
+          (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
+    
+        this.modelQuery = this.modelQuery.select(fields);
         return this;
       }
     
