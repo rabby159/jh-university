906b96a786b59741a14a81c435f9baf8018e5240
rabby159 <ablazerabbi159@gmail.com>
2024-08-22 03:05:58 +0600

    query builder and student service

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -5,90 +5,28 @@ import httpStatus from 'http-status'
 import { User } from '../user/user.model'
 import { TStudent } from './student.interface'
 import { studentSearchableFields } from './student.constant'
+import QueryBuilder from '../../builder/QueryBuilder'
 
 const getAllStudentFromDB = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query } // copying req.query object so that we can mutate the copy object
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-  let searchTerm = ''
-
-  // IF searchTerm  IS GIVEN SET IT
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string
-  }
-
-  // WE ARE DYNAMICALLY DOING IT USING LOOP
-  const searchQuery = Student.find({
-    $or: studentSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    })),
-  })
-
-  // FILTERING fUNCTIONALITY:
-
-  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields']
-  excludeFields.forEach((el) => delete queryObj[el]) // DELETING THE FIELDS SO THAT IT CAN'T MATCH OR FILTER EXACTLY
-
-  const filterQuery = searchQuery
-    .find(queryObj)
-    .populate('admissionSemester')
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    })
-
-  // SORTING FUNCTIONALITY:
-
-  let sort = '-createdAt' // SET DEFAULT VALUE
-
-  // IF sort  IS GIVEN SET IT
-
-  if (query.sort) {
-    sort = query.sort as string
-  }
-
-  const sortQuery = filterQuery.sort(sort)
-
-  // PAGINATION FUNCTIONALITY:
-
-  let page = 1 // SET DEFAULT VALUE FOR PAGE
-  let limit = 1 // SET DEFAULT VALUE FOR LIMIT
-  let skip = 0 // SET DEFAULT VALUE FOR SKIP
-  
-  // IF limit IS GIVEN SET IT
-  if (query.limit) {
-    limit = Number(query.limit)
-  }
-
-  // IF page IS GIVEN SET IT
-  if (query.page) {
-    page = Number(query.page)
-    skip = (page - 1) * limit
-  }
-
-  const paginateQuery = sortQuery.skip(skip);
-
-  const limitQuery = paginateQuery.limit(limit);
-  
-  
-  // FIELDS LIMITING FUNCTIONALITY:
-
-  // HOW OUR FORMAT SHOULD BE FOR PARTIAL MATCH 
-
-  //fields: 'name,email'; // ACCEPTING FROM REQUEST
-  //fields: 'name email'; // HOW IT SHOULD 
-
-  let fields = '-__v'; // SET DEFAULT VALUE
-
-  if (query.fields) {
-    fields = (query.fields as string).split(',').join(' ');
-
-  }
-
-  const fieldQuery = await limitQuery.select(fields);
-
-  return fieldQuery
+  const result = await studentQuery.modelQuery;
+  return result;
 }
 
 const getSingleStudentFromDB = async (id: string) => {
