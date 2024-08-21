b5ee049d813e7182ef5754f3bdf92745b725321c
Md Rabby <ablazerabbi159@gmail.com>
2024-08-21 21:27:20 +0600

    searhable field implement

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -4,5 +4,9 @@ class QueryBuilder<T> {
   public modelQuery: Query<T[], T>
   public query: Record<string, unknown>
 
-  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>)
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    ;(this.modelQuery = modelQuery), (this.query = query)
+  }
+
+  search(searchableFields: string[])
 }
