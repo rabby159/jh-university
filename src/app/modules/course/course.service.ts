661ccff03352cfae17cf394221235c10cad127c2
rabby159 <ablazerabbi159@gmail.com>
2024-08-24 05:25:28 +0600

    create course service

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,38 @@
+import { Course } from "./course.model"
+
+
+const createCourseIntoDB = async() => {
+    const result = await Course.create();
+
+    return result;
+};
+
+const getAllCoursesFromDB = async() => {
+    const result = await Course.find();
+
+    return result;
+};
+
+const getSingleCourseFromDB = async(id: string) => {
+    const result = await Course.findById(id);
+
+    return result;
+};
+
+const deleteCoursesFromDB = async(id: string) => {
+    const result = await Course.findByIdAndUpdate(
+        id, 
+        {isDeleted : true},
+        {new : true}
+    );
+
+    return result;
+}
+
+
+export const CourseServices = {
+    createCourseIntoDB,
+    getAllCoursesFromDB,
+    getSingleCourseFromDB,
+    deleteCoursesFromDB,
+}
