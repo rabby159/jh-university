29e92931197dd28f9f096d3da3f355c9a85c2e71
rabby159 <ablazerabbi159@gmail.com>
2024-08-24 02:51:30 +0600

    user utils

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import validateRequest from '../../middlewares/validatedRequest'
 import { studentValidations } from '../student/student.validation'
 import { facultyValidations } from '../faculty/faculty.validation'
+import { adminValidations } from '../admin/admin.validation'
 
 const router = express.Router()
 
@@ -16,6 +17,12 @@ router.post(
   '/create-faculty',
   validateRequest(facultyValidations.createFacultyValidationSchema),
   UserControllers.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(adminValidations.createAdminValidationSchema),
+  UserControllers.createAdmin,
 );
 
 export const UserRoutes = router
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -11,6 +11,7 @@ import httpStatus from 'http-status'
 import { AcademicDepartment } from '../academicDepartment/academicDepartment.model'
 import { TFaculty } from '../faculty/faculty.interface'
 import { Faculty } from '../faculty/faculty.model'
+import { TAdmin } from '../admin/admin.interface'
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user object
@@ -97,6 +98,56 @@ const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
     session.startTransaction();
     //set  generated id
     userData.id = await generateFacultyId();
+
+
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session }); // array
+
+    //create a faculty
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+    // set id , _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
+
+    // create a faculty (transaction-2)
+
+    const newFaculty = await Faculty.create([payload], { session });
+
+    if (!newFaculty.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create faculty');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newFaculty;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+};
+
+const createAdminIntoDB = async (password: string, payload: TAdmin) => {
+  // create a user object
+  const userData: Partial<TUser> = {};
+
+  //if password is not given , use deafult password
+  userData.password = password || (config.default_pass as string);
+
+  //set student role
+  userData.role = 'admin';
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //set  generated id
+    userData.id = await generateAdminId();
 
 
 
