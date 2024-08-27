875b2acde94439362f1a1f2ec53f2eb481852a7f
rabby159 <ablazerabbi159@gmail.com>
2024-08-28 05:07:32 +0600

    setup course validation for assign faculties

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -7,21 +7,25 @@ const router = express.Router()
 
 router.post(
   '/create-course',
-  validateRequest(
-    CourseValidations.createCourseValidationSchema,
-  ),
+  validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 )
 
 router.get('/:id', CourseControllers.getSingleCourse)
 
-router.patch('/:id', validateRequest(CourseValidations.updateCourseValidationSchema), 
-CourseControllers.updateCourse,
+router.patch(
+  '/:id',
+  validateRequest(CourseValidations.updateCourseValidationSchema),
+  CourseControllers.updateCourse,
 )
 
 router.delete('/:id', CourseControllers.deleteCourse)
 
-router.put('/:courseId/assign-faculties', CourseControllers.assignFaculties)
+router.put(
+  '/:courseId/assign-faculties',
+  validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.assignFaculties,
+)
 
 router.get('/', CourseControllers.getAllCourses)
 
--- src/app/modules/course/course.validation.ts
+++ src/app/modules/course/course.validation.ts
@@ -1,9 +1,9 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const PreRequisiteCourseValidationSchema = z.object({
   course: z.string(),
   isDeleted: z.boolean().optional(),
-});
+})
 
 const createCourseValidationSchema = z.object({
   body: z.object({
@@ -14,12 +14,12 @@ const createCourseValidationSchema = z.object({
     preRequisiteCourses: z.array(PreRequisiteCourseValidationSchema).optional(),
     isDeleted: z.boolean().optional(),
   }),
-});
+})
 
 const updatePreRequisiteCourseValidationSchema = z.object({
   course: z.string(),
   isDeleted: z.boolean().optional(),
-});
+})
 
 const updateCourseValidationSchema = z.object({
   body: z.object({
@@ -32,9 +32,16 @@ const updateCourseValidationSchema = z.object({
       .optional(),
     isDeleted: z.boolean().optional(),
   }),
-});
+})
+
+const facultiesWithCourseValidationSchema = z.object({
+  body: z.object({
+    faculties: z.array(z.string()),
+  }),
+})
 
 export const CourseValidations = {
-    createCourseValidationSchema,
-    updateCourseValidationSchema,
-  };
+  createCourseValidationSchema,
+  updateCourseValidationSchema,
+  facultiesWithCourseValidationSchema,
+}
