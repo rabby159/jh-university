545bcda1f7321f200a13cc8575861696367230ed
rabby159 <ablazerabbi159@gmail.com>
2024-08-31 02:21:48 +0600

    authentication service and utils create

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import config from '../../config';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
+import { AuthServices } from './auth.service';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
